f1e89fe3d2570653819c544e59e6f52313904e09
Jiranathanan Siripun <jiranathanan@gmail.com>
2026-01-06 22:29:53 +0700

    Detect collisions and restart when dies

---
+++ scenes/game.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+onready var felpudo = get_node("Felpudo")
+onready var timereplay = get_node("TimeToReplay")
+
+var pontos = 0 # Points
+var estado = 1 # State
+
+const JOGANDO = 1 # Playing
+const PERDENDO = 2 # Losing
+
+func _ready():
+	pass
+
+func kill():
+	felpudo.apply_impulse(Vector2(0, 0), Vector2(-1000, 0))
+	get_node("BackAnim").stop()
+	estado = PERDENDO # State = Losing
+	timereplay.start()
+	
+	
+
+func _on_TimeToReplay_timeout():
+	get_tree().reload_current_scene()
--- scripts/cano.gd
+++ scripts/cano.gd
@@ -1,13 +1,21 @@
 extends Node2D
 export var vel = -400
+var cena
 
 func _ready():
+	cena = get_tree().get_current_scene()
 	set_process(true)
 	
 func _process(delta):
-	set_pos(get_pos() + Vector2(vel * delta, 0))
+	if cena.estado == cena.JOGANDO: # if scene.state == scene.PLAYING
+		set_pos(get_pos() + Vector2(vel * delta, 0))
 	
 	if get_pos().x < -100:
 		print("Destroyed")
 		queue_free()
 		
+
+
+func _on_Area2D_body_enter( body ):
+	if body.get_name() == "Felpudo":
+		cena.kill()
